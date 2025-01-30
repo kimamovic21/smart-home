89715b932bb6ff6b21754d01dfe53040bf0a9677
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-30 21:50:01 +0100

    SmartHome class added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SmartHomeApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            SmartHome smartHome = new SmartHome();
+            smartHome.WelcomeMessage();
+        }
+    }
+}
---
+++ SmartHome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartHomeApp
+{
+    public class SmartHome
+    {
+        public void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Smart Home");
+        }
+    }
+}
