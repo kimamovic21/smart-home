c7c058f7b47801ee2b712fa15d0e88a468002c93
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-31 10:57:02 +0100

    SmartAlarm class added

--- Program.cs
+++ Program.cs
@@ -8,6 +8,20 @@ namespace SmartHomeApp
         {
             SmartHome smartHome = new SmartHome();
             smartHome.WelcomeMessage();
+
+            SmartAlarm alarm = new SmartAlarm();
+
+            Console.WriteLine("\nTurning on the alarm...");
+            alarm.TurnOnAlarm();
+
+            Console.WriteLine("\nChecking alarm status...");
+            alarm.GetAlarmStatus();
+
+            Console.WriteLine("\nTurning off the alarm...");
+            alarm.TurnOffAlarm();
+
+            Console.WriteLine("\nChecking alarm status again...");
+            alarm.GetAlarmStatus();
         }
     }
 }
---
+++ SmartAlarm.cs
@@ -0,0 +1,25 @@
+﻿namespace SmartHomeApp
+{
+    internal class SmartAlarm
+    {
+        private bool isAlarmOn;
+
+        public void TurnOnAlarm()
+        {
+            isAlarmOn = true;
+            Console.WriteLine("Alarm is now ON!");
+        }
+
+        public void TurnOffAlarm()
+        {
+            isAlarmOn = false;
+            Console.WriteLine("Alarm is now OFF!");
+        }
+
+        public void GetAlarmStatus()
+        {
+            string status = isAlarmOn ? "ON" : "OFF";
+            Console.WriteLine($"Current alarm status: {status}");
+        }
+    }
+}
