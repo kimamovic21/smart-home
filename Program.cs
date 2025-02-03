211ae0b2c0c13a9f64ae9f18176cdffe7d25c558
kimamovic21 <imamovic.kerim@gmail.com>
2025-02-03 22:39:10 +0100

    SmartDoorLock class added

--- Program.cs
+++ Program.cs
@@ -9,10 +9,13 @@ namespace SmartHomeApp
             SmartAlarm smartAlarm = new SmartAlarm();
             SmartTV smartTV = new SmartTV();
             SmartHeating smartHeating = new SmartHeating();
+            SmartDoorLock smartDoorLock = new SmartDoorLock(); 
 
             Console.WriteLine("Smart alarm id is: " + smartAlarm.SmartDeviceId);
             Console.WriteLine("Smart TV id is: " + smartTV.SmartDeviceId);
             Console.WriteLine("Smart heating id is: " + smartHeating.SmartDeviceId);
+            Console.WriteLine("Smart door lock id is: " + smartDoorLock.SmartDeviceId);
+
             while (true)
             {
                 int electricityConsumptionBySmartHome = smartHeating.GetElectricityConsumption();
@@ -21,31 +24,26 @@ namespace SmartHomeApp
                 Console.WriteLine("1. Change status of smart alarm");
                 Console.WriteLine("2. Change status of the smart TV");
                 Console.WriteLine("3. Change status of the smart heating");
-                Console.WriteLine("4. Exit");
-                Console.WriteLine("\nTotal electricity consumption is: " + electricityConsumptionBySmartHome+ "kWh");
-                if (electricityConsumptionBySmartHome > 100)
+                Console.WriteLine("4. Change status of the smart door lock"); 
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nTotal electricity consumption is: " + electricityConsumptionBySmartHome + "kWh");
+
+                if (electricityConsumptionBySmartHome > 150)
                 {
-                    Console.WriteLine("Warning! Electricity consumption is above 100kWh");
+                    Console.WriteLine("Warning! Electricity consumption is above 150kWh");
                 }
+
                 string? userInput = Console.ReadLine();
+
                 if (userInput == "1")
                 {
                     Console.WriteLine("Choose an option");
                     Console.WriteLine("1. Turn on the smart alarm");
                     Console.WriteLine("2. Turn off the smart alarm");
                     string? alarmInput = Console.ReadLine();
-                    if (alarmInput == "1")
-                    {
-                        smartAlarm.TurnOn();
-                    }
-                    else if (alarmInput == "2")
-                    {
-                        smartAlarm.TurnOff();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                    }
+                    if (alarmInput == "1") smartAlarm.TurnOn();
+                    else if (alarmInput == "2") smartAlarm.TurnOff();
+                    else Console.WriteLine("Invalid input");
                 }
                 else if (userInput == "2")
                 {
@@ -53,18 +51,9 @@ namespace SmartHomeApp
                     Console.WriteLine("1. Turn on the smart TV");
                     Console.WriteLine("2. Turn off the smart TV");
                     string? tvInput = Console.ReadLine();
-                    if (tvInput == "1")
-                    {
-                        smartTV.TurnOn();
-                    }
-                    else if (tvInput == "2")
-                    {
-                        smartTV.TurnOff();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                    }
+                    if (tvInput == "1") smartTV.TurnOn();
+                    else if (tvInput == "2") smartTV.TurnOff();
+                    else Console.WriteLine("Invalid input");
                 }
                 else if (userInput == "3")
                 {
@@ -73,14 +62,8 @@ namespace SmartHomeApp
                     Console.WriteLine("2. Turn off the smart heating");
                     Console.WriteLine("3. Set the temperature of the smart heating");
                     string? heatingInput = Console.ReadLine();
-                    if (heatingInput == "1")
-                    {
-                        smartHeating.TurnOn();
-                    }
-                    else if (heatingInput == "2")
-                    {
-                        smartHeating.TurnOff();
-                    }
+                    if (heatingInput == "1") smartHeating.TurnOn();
+                    else if (heatingInput == "2") smartHeating.TurnOff();
                     else if (heatingInput == "3")
                     {
                         Console.WriteLine("Enter the desired temperature (0 - 30°C)");
@@ -89,17 +72,48 @@ namespace SmartHomeApp
                         {
                             smartHeating.SetTemperature(temperature);
                         }
-                        else
-                        {
-                            Console.WriteLine("Invalid input");
-                        }
+                        else Console.WriteLine("Invalid input");
+                    }
+                    else Console.WriteLine("Invalid input");
+                }
+                else if (userInput == "4") 
+                {
+                    Console.WriteLine("Choose an option");
+                    Console.WriteLine("1. Lock the door");
+                    Console.WriteLine("2. Unlock the door");
+                    Console.WriteLine("3. Change PIN code");
+                    Console.WriteLine("4. Check lock status");
+
+                    string? doorInput = Console.ReadLine();
+
+                    if (doorInput == "1")
+                    {
+                        smartDoorLock.LockDoor();
+                    }
+                    else if (doorInput == "2")
+                    {
+                        Console.Write("Enter PIN to unlock: ");
+                        string? enteredPin = Console.ReadLine();
+                        smartDoorLock.UnlockDoor(enteredPin);
+                    }
+                    else if (doorInput == "3")
+                    {
+                        Console.Write("Enter old PIN: ");
+                        string? oldPin = Console.ReadLine();
+                        Console.Write("Enter new PIN: ");
+                        string? newPin = Console.ReadLine();
+                        smartDoorLock.ChangePinCode(oldPin, newPin);
+                    }
+                    else if (doorInput == "4")
+                    {
+                        smartDoorLock.GetStatus();
                     }
                     else
                     {
                         Console.WriteLine("Invalid input");
                     }
                 }
-                else if (userInput == "4")
+                else if (userInput == "5")
                 {
                     break;
                 }
@@ -107,7 +121,7 @@ namespace SmartHomeApp
                 {
                     Console.WriteLine("Invalid input");
                 }
-            }          
+            }
         }
     }
 }
---
+++ Smart Devices/SmartDoorLock.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SmartHomeApp
+{
+    class SmartDoorLock : SmartHome
+    {
+        private bool isLocked = true;
+        private string pinCode = "1234";  
+        private int requiredElectricity = 10;  
+
+        public bool IsLocked { get => isLocked; }
+        public string PinCode { get => pinCode; }
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Smart door lock is now active.");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Smart door lock is now inactive.");
+        }
+
+        public override bool GetStatus()
+        {
+            string lockStatus = isLocked ? "locked" : "unlocked";
+            Console.WriteLine($"The door is currently {lockStatus}.");
+            return isLocked;
+        }
+
+        public void LockDoor()
+        {
+            if (!isLocked)
+            {
+                isLocked = true;
+                ElectricityConsumption += requiredElectricity;
+                Console.WriteLine("Door is now locked.");
+            }
+            else
+            {
+                Console.WriteLine("Door is already locked.");
+            }
+        }
+
+        public void UnlockDoor(string enteredPin)
+        {
+            if (enteredPin == pinCode)
+            {
+                if (isLocked)
+                {
+                    isLocked = false;
+                    Console.WriteLine("Door is now unlocked.");
+                }
+                else
+                {
+                    Console.WriteLine("Door is already unlocked.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect PIN! Door remains locked.");
+            }
+        }
+
+        public void ChangePinCode(string oldPin, string newPin)
+        {
+            if (oldPin == pinCode)
+            {
+                pinCode = newPin;
+                Console.WriteLine("PIN code has been changed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect old PIN! Unable to change PIN.");
+            }
+        }
+    }
+}
